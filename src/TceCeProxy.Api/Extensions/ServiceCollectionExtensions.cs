95d8de850770c91bc425d9e8fe5f4ed0c79381af
jvyctor <joaovyctor1301@hotmail.com>
2026-03-31 15:58:38 -0300

    Force IPv4 upstream connections in API client

--- src/TceCeProxy.Api/Extensions/ServiceCollectionExtensions.cs
+++ src/TceCeProxy.Api/Extensions/ServiceCollectionExtensions.cs
@@ -1,3 +1,6 @@
+using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using TceCeProxy.Api.Options;
 using TceCeProxy.Api.Services;
@@ -19,9 +22,44 @@ public static class ServiceCollectionExtensions
         services.AddResponseCompression();
         services.AddSingleton<ITceCeResourceCatalog, TceCeSwaggerResourceCatalog>();
         services.AddHttpClient<ITceCeClient, TceCeClient>(client =>
-        {
-            client.Timeout = TimeSpan.FromSeconds(30);
-        });
+            {
+                client.Timeout = TimeSpan.FromSeconds(45);
+                client.DefaultRequestVersion = HttpVersion.Version11;
+                client.DefaultVersionPolicy = HttpVersionPolicy.RequestVersionOrLower;
+            })
+            .ConfigurePrimaryHttpMessageHandler(() => new SocketsHttpHandler
+            {
+                AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate,
+                ConnectTimeout = TimeSpan.FromSeconds(15),
+                PooledConnectionLifetime = TimeSpan.FromMinutes(5),
+                EnableMultipleHttp2Connections = false,
+                ConnectCallback = async (context, cancellationToken) =>
+                {
+                    var addresses = await Dns.GetHostAddressesAsync(context.DnsEndPoint.Host, cancellationToken);
+                    var ipv4Address = addresses.FirstOrDefault(address => address.AddressFamily == AddressFamily.InterNetwork);
+
+                    if (ipv4Address is null)
+                    {
+                        throw new SocketException((int)SocketError.HostNotFound);
+                    }
+
+                    var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                    try
+                    {
+                        await socket.ConnectAsync(
+                            new IPEndPoint(ipv4Address, context.DnsEndPoint.Port),
+                            cancellationToken);
+
+                        return new NetworkStream(socket, ownsSocket: true);
+                    }
+                    catch
+                    {
+                        socket.Dispose();
+                        throw;
+                    }
+                }
+            });
         services.AddHealthChecks()
             .AddCheck("self", () => HealthCheckResult.Healthy(), tags: ["live"])
             .AddCheck<ResourceCatalogHealthCheck>("resource_catalog", tags: ["ready"]);
